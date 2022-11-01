d5ed03a2ee9a59d636f84ff2454ba419c8af0895
puppy2111 <huynhdaihuybank3@gmail.com>
2022-11-01 11:51:11 +0700

    Contactbook frontend

---
+++ contactbook-frontend/src/components/RegisterForm.vue
@@ -0,0 +1,112 @@
+<template>
+  <Form @submit="submitContact" :validation-schema="contactFormSchema">
+    <div class="form-group">
+      <label for="name">Tên</label>
+      <Field name="name" type="text" class="form-control" v-model="nameInput" />
+      <ErrorMessage name="name" class="error-feedback" />
+    </div>
+    <div class="form-group">
+      <label for="email">E-mail</label>
+      <Field
+        name="email"
+        type="email"
+        class="form-control"
+        v-model="emailInput"
+      />
+      <ErrorMessage name="email" class="error-feedback" />
+    </div>
+    <div class="form-group">
+      <label for="address">Địa chỉ</label>
+      <Field
+        name="address"
+        type="text"
+        class="form-control"
+        v-model="addressInput"
+      />
+      <ErrorMessage name="address" class="error-feedback" />
+    </div>
+    <div class="form-group">
+      <label for="phone">Điện thoại</label>
+      <Field
+        name="phone"
+        type="tel"
+        class="form-control"
+        v-model="phoneInput"
+      />
+      <ErrorMessage name="phone" class="error-feedback" />
+    </div>
+    <div class="form-group form-check">
+      <input
+        name="favorite"
+        type="checkbox"
+        class="form-check-input"
+        v-model="favoriteInput"
+      />
+      <label for="favorite" class="form-check-label">
+        <strong>Liên hệ yêu thích</strong>
+      </label>
+    </div>
+    <div class="form-group">
+      <button class="btn btn-primary">Tạo</button>
+    </div>
+  </Form>
+</template>
+  <script>
+import * as yup from "yup";
+import { Form, Field, ErrorMessage } from "vee-validate";
+export default {
+  components: {
+    Form,
+    Field,
+    ErrorMessage,
+  },
+  emits: ["submit:contact"],
+  data() {
+    const contactFormSchema = yup.object().shape({
+      name: yup
+        .string()
+        .required("Tên phải có giá trị.")
+        .min(2, "Tên phải ít nhất 2 ký tự.")
+        .max(50, "Tên có nhiều nhất 50 ký tự."),
+      email: yup
+        .string()
+        .email("E-mail không đúng.")
+        .max(50, "E-mail tối đa 50 ký tự."),
+      address: yup.string().max(100, "Địa chỉ tối đa 100 ký tự."),
+      phone: yup
+        .string()
+        .matches(
+          /((09|03|07|08|05)+([0-9]{8})\b)/g,
+          "Số điện thoại không hợp lệ."
+        ),
+    });
+    return {
+      // Chúng ta sẽ không muốn hiệu chỉnh props, nên tạo biến cục bộ
+      // contactLocal để liên kết với các input trên form
+      nameInput: "",
+      emailInput: "",
+      addressInput: "",
+      phoneInput: "",
+      favoriteInput: "",
+      contactFormSchema,
+    };
+  },
+  methods: {
+    submitContact() {
+      const dataForm = {
+        name: this.nameInput,
+        email: this.emailInput,
+        address: this.addressInput,
+        phone: this.phoneInput,
+        favorite: this.favoriteInput,
+      };
+      const jsonString = JSON.stringify(dataForm);
+      const dataBinding = JSON.parse(jsonString);
+      this.$emit("submit:contact", dataBinding);
+    },
+  },
+};
+</script>
+<style scoped>
+@import "@/assets/form.css";
+</style>
---
+++ contactbook-frontend/src/views/ContactAdd.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="page">
+    <h4>Hiệu chỉnh Liên hệ</h4>
+    <RegisterForm @submit:contact="createContact" />
+    <p>{{ message }}</p>
+  </div>
+</template>
+  <script>
+import RegisterForm from "../components/RegisterForm.vue";
+import ContactService from "@/services/contact.service";
+export default {
+  components: {
+    RegisterForm,
+  },
+  props: {
+    contact: { type: Object, default: null },
+  },
+  data() {
+    return {
+      message: "",
+    };
+  },
+  methods: {
+    async createContact(contact) {
+      try {
+        console.log(contact);
+        await ContactService.create(contact);
+        this.message = "Liên hệ được tạo thành công.";
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+  create() {
+    this.message = "";
+  },
+};
+</script>  
